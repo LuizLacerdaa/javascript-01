d2401510fa136de7f54e9b2c3f2f433aa20d1231
LuizLacerdaa <luizlacerda.r@gmail.com>
2025-03-31 19:14:01 -0300

    Refratoração de Código

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
-console.log ("Luiz")
-console.log (21)
- 
-// formas de criar variaveis
-// var, let, const
-
-let nome = "Luiz"
-let idade = "21"
-console.log (nome, idade)
-nome = "Luiz Lacerda"
-console.log (nome)
-
 let nomeCompleto = ("")
 nomeCompleto = prompt ("informe seu nome completo:")
-console.log (nomeCompleto)
 
-//alert("Seja Bem Vindo(a) " + nomeCompleto)
-
-//document.write("Seja Bem Vindo(a) " + nomeCompleto)
 let cor = prompt ("Informe uma cor: \n 1-azul \n 2-Vermelho \n 3-preto \n 4-Verde \n 5-Rosa \n 6-Laranja")
-
-
 let corDeFundo = null
 
 if(cor == "1"){
@@ -43,8 +25,6 @@ else if(cor == "6"){
 else{
     corDeFundo = "yellow"
 }
-
-
 
 document.body.style.backgroundColor = corDeFundo
 

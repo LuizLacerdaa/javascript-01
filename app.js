e93dc9809649dd8dea359602864e068054ccd3d6
LuizLacerdaa <luizlacerda.r@gmail.com>
2025-03-31 19:53:48 -0300

    exercicio 01
    Crie um programa que peça para o usuário digitar 2 números(prompt), some esses números e mostre o resultado em um alert

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 let nomeCompleto = ("")
-nomeCompleto = prompt ("informe seu nome completo:")
+nomeCompleto = prompt("informe seu nome completo:")
 
-let cor = prompt ("Informe uma cor: \n 1-azul \n 2-Vermelho \n 3-preto \n 4-Verde \n 5-Rosa \n 6-Laranja")
-let corDeFundo = null
+/*
 
-if(cor == "1"){
-    corDeFundo = "blue"
-}
-else if(cor =="2"){
-    corDeFundo = "red"
-}
-else if(cor == "3"){
-    corDeFundo = "black"
-}
-else if(cor =="4"){
-    corDeFundo = "green"
-}
-else if(cor == "5"){
-    corDeFundo = "pink"
-}
-else if(cor == "6"){
-    corDeFundo = "orange"
-}
-else{
-    corDeFundo = "yellow"
+function fnTrocarCorDeFundo(){
+
+    let cor = prompt("Informe uma cor: \n 1-azul \n 2-Vermelho \n 3-preto \n 4-Verde \n 5-Rosa \n 6-Laranja")
+    let corDeFundo = null
+
+    if (cor == "1") {
+        corDeFundo = "blue"
+    }
+    else if (cor == "2") {
+        corDeFundo = "red"
+    }
+    else if (cor == "3") {
+        corDeFundo = "black"
+    }
+    else if (cor == "4") {
+        corDeFundo = "green"
+    }
+    else if (cor == "5") {
+        corDeFundo = "pink"
+    }
+    else if (cor == "6") {
+        corDeFundo = "orange"
+    }
+    else {
+        corDeFundo = "yellow"
+    }
+
+    document.body.style.backgroundColor = corDeFundo
+
 }
 
-document.body.style.backgroundColor = corDeFundo
 
+fnTrocarCorDeFundo() 
+
+*/
---
+++ exercicio-01/ex01.js
@@ -0,0 +1,8 @@
+let num1 = null
+let num2 = null
+
+num1 = parseFloat(prompt("Digite o primeiro numero: "))
+num2 = parseFloat(prompt("Digite o segundo numero"))
+
+alert ("o resultado é: " + (num1 + num2)) 
+

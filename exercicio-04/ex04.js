a6441016f4ee65880ea4185edead728d11d82172
LuizLacerdaa <luizlacerda.r@gmail.com>
2025-03-31 21:23:33 -0300

    exercicio 4
    Faça um programa que calcule a média simples (aritmética) de 3 valores quaisquer informado pelo usuário. Utilize as variáveis valor1, valor2 e valor3. Exiba a média na tela para o usuário.

---
+++ exercicio-04/ex04.js
@@ -0,0 +1,6 @@
+let valor1 = parseFloat(prompt("Digite o primeiro numero"))
+let valor2 = parseFloat(prompt("Digite o segundo numero"))
+let valor3 = parseFloat(prompt("Digite o terceiro numero"))
+media = (valor1 + valor2 + valor3) / 3
+
+alert ("A média dos numeros digitados é: " + media)

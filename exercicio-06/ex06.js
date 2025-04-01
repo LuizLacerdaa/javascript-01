c250285d718d3f3d8115656292afc7e3b4058eea
LuizLacerdaa <luizlacerda.r@gmail.com>
2025-03-31 21:33:11 -0300

    Exercicio 6
    Escreva um programa para mostrar o sucessor e o antecessor de um número qualquer informado pelo usuário. Exiba na tela os números seguindo o exemplo da mensagem: “O antecessor do número 10 é 9, e o sucessor é 11.”.

---
+++ exercicio-06/ex06.js
@@ -0,0 +1,3 @@
+let valor1 = parseFloat(prompt("Insira um numero para saber o antecessor e o sucessor dele."))
+
+alert ("O antecessor do numero " + valor1 + " é: " + (valor1 - 1) + "e o sucesor é o numero:" + (valor + 1 )+".")
